898212ae6280e4bbbb84abcf0613ac01c8530f01
zhangjianming1985 <78098702@qq.com>
2020-01-15 17:45:47 +0800

    add by zhangjm

--- product-server/src/main/java/com/kunlun/erp/core/log/ControllerLogAspect.java
+++ product-server/src/main/java/com/kunlun/erp/core/log/ControllerLogAspect.java
@@ -1,13 +1,14 @@
 package com.kunlun.erp.core.log;
 
-import com.alibaba.fastjson.JSONObject;
 import com.kunlun.erp.core.common.constants.SysConstant;
 import com.kunlun.erp.core.common.constants.Urls;
-import com.kunlun.erp.core.common.util.DateUtil;
 import com.kunlun.erp.core.common.json.JsonUtil;
+import com.kunlun.erp.core.common.util.DateUtil;
 import com.kunlun.erp.core.common.util.LogUtil;
 import com.kunlun.erp.core.dto.AbstractRequest;
 import com.kunlun.erp.core.dto.AbstractResponse;
+import com.kunlun.erp.core.dto.user.UserInfoRespDto;
+import com.kunlun.erp.core.service.account.AccountService;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -27,6 +29,8 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 @Aspect
 public class ControllerLogAspect {
+    @Resource(name = "account_service")
+    private AccountService account_service;
     @Around("within(com.kunlun.erp.core.controller..*) && @annotation(systemLog)")
     public Object doAroundMethod(ProceedingJoinPoint pjd, SystemLog systemLog) throws Throwable {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
@@ -57,13 +61,18 @@ public class ControllerLogAspect {
         sb.append("[request message]:").append("\n");
         sb.append("	[call method]:").append(methodName).append("\n");
         sb.append("	[begin time]:").append(DateUtil.millisToStringByFormat(DateUtil.FORMATTER_DATE_TIME_DETAIL, Long.valueOf(beginDate))).append("\n");
+        String login_name="";
+        String user_name="";
         // 处理请求参数
         if (reqParams != null && reqParams[0] instanceof AbstractRequest) {
-            JSONObject json_object = JsonUtil.toJsonObj(reqParams[0]);
-            JSONObject header_obj = json_object.getJSONObject("header");
-            sb.append("	[req header]:").append(JsonUtil.toStr(header_obj)).append("\n");
-            JSONObject body_obj = json_object.getJSONObject("body");
-            sb.append("	[req body]:").append(JsonUtil.toStr(body_obj)).append("\n");
+            AbstractRequest request = (AbstractRequest)reqParams[0];
+            AbstractResponse<UserInfoRespDto> user_info = account_service.getUserInfo(request.getHeader().getTrans_no(),request.getHeader().getSecret_key(),Urls.USER_NAME_SPACE);
+            if (user_info!=null && user_info.getHeader().getState().equals(SysConstant.RespStatus.resp_status_success.getValue())){
+                login_name=user_info.getBody().getLoginName();
+                user_name=user_info.getBody().getUserName();
+            }
+            sb.append("	[req header]:").append(JsonUtil.toStr(request.getHeader())).append("-"+login_name+"|"+user_name).append("\n");
+            sb.append("	[req body]:").append(JsonUtil.toStr(request.getBody())).append("\n");
             sb.append("\n");
         }else {
             sb.append("	[request content]:").append(JsonUtil.toStr(reqParams)).append("\n");
@@ -71,20 +80,17 @@ public class ControllerLogAspect {
         sb.append("[response message]:").append("\n");
         // 处理响应结果
         if (respDto instanceof AbstractResponse) {
-            JSONObject jobject = JsonUtil.toJsonObj(respDto);
-            JSONObject header_obj = jobject.getJSONObject("header");
-            sb.append("	[resp header]:").append(JsonUtil.toStr(header_obj)).append("\n");
-            JSONObject body_obj = jobject.getJSONObject("body");
+            AbstractResponse response = (AbstractResponse)respDto;
+            sb.append("	[resp header]:").append(JsonUtil.toStr(response.getHeader())).append("-"+login_name+"|"+user_name).append("\n");
             sb.append("	[resp body]:");
             if (systemLog.show_resp_body()){
-                sb.append(JsonUtil.toStr(body_obj)).append("\n");
+                sb.append(JsonUtil.toStr(response.getBody())).append("\n");
             }else{
                 sb.append("not show").append("\n");
             }
-            if (header_obj.getString("state").equalsIgnoreCase(SysConstant.RespStatus.resp_status_fail.getValue())){
+            if (response.getHeader().getState().equals(SysConstant.RespStatus.resp_status_fail.getValue())){
                 log_level = SysConstant.LogLevel.error;
             }
-
         }else {
             sb.append("	[response content]:").append(JsonUtil.toStr(respDto)).append("\n");
         }
